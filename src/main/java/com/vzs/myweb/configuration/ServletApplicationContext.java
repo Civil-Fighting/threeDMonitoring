c572241a8aaf4d99acfd440bcd4635b2da8f644d
byao <byao@coupang.com>
2017-08-02 13:21:47 +0800

    xx

--- src/main/java/com/vzs/myweb/configuration/ServletApplicationContext.java
+++ src/main/java/com/vzs/myweb/configuration/ServletApplicationContext.java
@@ -5,6 +5,8 @@ import com.vzs.myweb.web.Webs;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 /**
@@ -15,4 +17,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 @VzsEnableHandlebar
 @ComponentScan(basePackageClasses = { Webs.class})
 public class ServletApplicationContext extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        this.addStaticResourceHandler(registry);
+        super.addResourceHandlers(registry);
+    }
+
+    private void addStaticResourceHandler(ResourceHandlerRegistry registry) {
+        String resourcePath = "/resources/**";
+        ResourceHandlerRegistration resourceHandlerRegistration = registry.addResourceHandler(new String[]{resourcePath});
+        resourceHandlerRegistration.addResourceLocations(new String[]{"/WEB-INF/resources/"});
+        resourceHandlerRegistration.setCachePeriod(Integer.valueOf(0));
+    }
 }
